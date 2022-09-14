af41141f04a989871241e87230bab063596ed23f
Valerii from earth <lvaldev.code@gmail.com>
2022-09-14 23:15:07 +0200

    add input validation

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import Button from "./components/UI/Button/Button";
 import Card from "./components/UI/Card/Card";
 import Input from "./components/UI/Input/Input";
 
+// b20a60cec7-107d28d747-ri7x5k
+
 function App() {
 	return (
 		<div className='App'>
 			<Card title='Konwerter walut'>
 				<Button>Konwertuj</Button>
 				<br />
-				<Input id='currency' label='PLN' />
+				<Input id='currency' label='PLN' name='Kwota' />
 			</Card>
 		</div>
 	);
--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -1,28 +1,35 @@
-import React, { FC, InputHTMLAttributes, useState } from "react";
+import React, { FC, useState } from "react";
 import styles from "./Input.module.css";
 
 interface InputProps {
 	label: string;
 	id: string;
 	placeholder?: string;
+	name: string;
 }
 
-const Input: FC<InputProps> = ({ placeholder, id, label }) => {
+const Input: FC<InputProps> = ({ placeholder, id, label, name }) => {
 	const [inputText, setInputText] = useState<string>("");
 	const [inputError, setInputError] = useState<boolean>(false);
 
 	const onBlurHandler = (): void => {
 		inputText.match(/[a-zA-Z]+/g) ? setInputError(true) : setInputError(false);
 	};
+	const onFocusHandler = (): void => {
+		inputText.match(/[a-zA-Z]+/g) ? setInputText("") : setInputText(inputText);
+		setInputError(false);
+	};
 
 	return (
 		<div className={`${styles.input} ${inputError ? styles.invalid : ""}`}>
+			<p>{name}</p>
 			<input
 				placeholder={placeholder}
 				id={id}
 				value={inputText}
 				onChange={(e) => setInputText(e.target.value)}
 				onBlur={onBlurHandler}
+				onFocus={onFocusHandler}
 			/>
 			<label htmlFor={id}>{label}</label>
 		</div>
