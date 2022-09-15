d2be36006857188883234b3e1f51756cb1482390
Valerii from earth <lvaldev.code@gmail.com>
2022-09-15 21:06:26 +0200

    add layout styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import Button from "./components/UI/Button/Button";
-import Card from "./components/UI/Card/Card";
-import Input from "./components/UI/Input/Input";
-import Select from "./components/UI/Select/Select";
+import Button from "./components/Button/Button";
+import Card from "./components/Card/Card";
+import Input from "./components/Input/Input";
+import Select from "./components/Select/Select";
+import arrowsIcon from "./assets/change.svg";
 
 // b20a60cec7-107d28d747-ri7x5k
 const optionsArr = [{ name: "PLN" }, { name: "USD" }, { name: "UAH" }, { name: "RUB" }, { name: "EUR" }];
@@ -10,15 +11,22 @@ function App() {
 	return (
 		<div className='App'>
 			<Card title='Konwerter walut'>
-				<div style={{ display: "flex" }}>
-					<Input id='from' label='PLN' name='Kwota' placeholder='Wpisz kwote' />
-					<Input id='to' label='USD' name='Wynik' placeholder='Wynik' disabled />
+				<div className='row'>
+					<div className='col'>
+						<Select title='Przelicz z' options={optionsArr}></Select>
+						<div className='change'>
+							<img src={arrowsIcon} alt='change icon' />
+						</div>
+						<Select title='Przelicz na' options={optionsArr}></Select>
+					</div>
+					<div className='col'>
+						<Input id='from' label='PLN' name='Kwota' placeholder='Wpisz kwote' />
+						<Input id='to' label='USD' name='Wynik' placeholder='Wynik' disabled />
+					</div>
 				</div>
-				<div style={{ display: "flex" }}>
+				<div className='controls'>
 					<Button view='secondary'>Ukryj historię</Button>
 					<Button>Konwertuj</Button>
-					<Select title='Przelicz z' options={optionsArr}></Select>
-					<Select title='Przelicz na' options={optionsArr}></Select>
 				</div>
 			</Card>
 		</div>
