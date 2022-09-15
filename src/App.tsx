f5836a0dd999a052ef676c24dd7a8da6386e1dd4
Valerii from earth <lvaldev.code@gmail.com>
2022-09-15 19:00:34 +0200

    add select component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import Button from "./components/UI/Button/Button";
 import Card from "./components/UI/Card/Card";
 import Input from "./components/UI/Input/Input";
+import Select from "./components/UI/Select/Select";
 
 // b20a60cec7-107d28d747-ri7x5k
-
+const optionsArr = [{ name: "PLN" }, { name: "USD" }, { name: "UAH" }, { name: "RUB" }, { name: "EUR" }];
 function App() {
 	return (
 		<div className='App'>
@@ -16,6 +17,8 @@ function App() {
 				<div style={{ display: "flex" }}>
 					<Button view='secondary'>Ukryj historię</Button>
 					<Button>Konwertuj</Button>
+					<Select title='Przelicz z' options={optionsArr}></Select>
+					<Select title='Przelicz na' options={optionsArr}></Select>
 				</div>
 			</Card>
 		</div>
--- src/components/UI/Select/Select.tsx
+++ src/components/UI/Select/Select.tsx
@@ -1,16 +1,43 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import styles from "./Select.module.css";
 
 interface SelectProps {
-	options: React.ReactElement[];
+	options: Option[];
+	title: string;
 }
 
-const Select: FC<SelectProps> = ({ options }) => {
+interface Option {
+	name: string;
+}
+
+const Select: FC<SelectProps> = ({ options, title }) => {
+	const [selectValue, setSelectValue] = useState<string>("PLN");
+	const [drop, setDrop] = useState<boolean>(false);
+
+	const clickItemHandler = (e: any) => {
+		console.log(e.target.textContent);
+		setSelectValue(e.target.textContent);
+	};
+
+	const dropListHandler = () => {
+		setDrop((p) => !p);
+	};
 	return (
-		<select>
-			{options.map((option) => (
-				<option>option.name</option>
-			))}
-		</select>
+		<div className={styles.item}>
+			<p className={styles.title}>{title}</p>
+			<div className={`${styles.select} ${drop ? styles.open : ""}`} onClick={dropListHandler}>
+				<span className={styles.current}>{selectValue}</span>
+				<div className={styles.dropdown}>
+					<ul>
+						{options.map((option) => (
+							<li key={option.name} id={option.name} onClick={clickItemHandler}>
+								{option.name}
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</div>
 	);
 };
 export default Select;
